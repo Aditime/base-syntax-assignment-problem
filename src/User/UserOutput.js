ee6f76519007b43339042b1ddb7369e858a85a86
Aditi <barua.aditi.cs@gmail.com>
2021-02-27 00:46:29 -0700

    first assignment on basic react

---
+++ src/User/UserOutput.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UserOutput = (props) =>{
+    return (
+        <div>
+            <p>This is the username: {props.username}</p>
+            <p>This is the username: {props.username}</p>
+        </div>
+    )};
+
+export default UserOutput;
